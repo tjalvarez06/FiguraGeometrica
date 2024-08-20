2f07cbb9ab23bd409aaa7383d61a470c24596b37
tjalvarez06 <tomasalvarez06@hotmail.com>
2024-08-20 07:06:04 -0400

    Add project files.

---
+++ FiguraGeometrica/Class/FiguraGeometrica.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Figura
+{
+    
+    public class FiguraGeometrica
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public FiguraGeometrica(double baseFigura, double altura)
+        {
+            Base = baseFigura;
+            Altura = altura;
+        }
+        public virtual double CalcularArea()
+        {
+            return Base * Altura;
+        }
+
+    }
+    public class Rectangulo : FiguraGeometrica
+    {
+        public Rectangulo(double baseFigura, double altura)
+            : base(baseFigura, altura)
+        {
+        }
+        public override double CalcularArea()
+        {
+            return Base * Altura;  
+        }
+    }
+    public class Cuadrado : FiguraGeometrica
+    {
+        public Cuadrado(double baseFigura, double altura)
+            : base(baseFigura, altura)
+        {
+        }
+        public override double CalcularArea()
+        {
+            return Base * Altura;
+        }
+        
+    }
+}
---
+++ FiguraGeometrica/Program.cs
@@ -0,0 +1,10 @@
+﻿using Figura;
+
+FiguraGeometrica figura = new FiguraGeometrica(10,5);
+Rectangulo rectangulo = new Rectangulo(10, 5);
+Cuadrado cuadrado = new Cuadrado(10,5);
+
+Console.WriteLine("Área de la FiguraGeometrica: " + figura.CalcularArea());
+Console.WriteLine("Área del Rectángulo: " + rectangulo.CalcularArea());
+Console.WriteLine("Área del Cuadrado (con error): " + cuadrado.CalcularArea());
+
